b7fed88826077c3c3232e2db3c0fbc50faf3c11b
Juan David Perez <juandavidperezvergara@gmail.com>
2023-10-13 17:07:25 -0500

    Añadiendo el modal y poniendole estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MateriasComponent } from './materias/materias.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MateriasComponent
+    MateriasComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/materias/materias.component.ts
+++ src/app/materias/materias.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-materias',
   templateUrl: './materias.component.html',
-  styleUrls: ['./materias.component.css']
+  styleUrls: ['./materias.component.css'],
 })
 export class MateriasComponent {
   selectedItem: string = 'Materias'; // Establece el valor predeterminado
@@ -15,4 +17,14 @@ export class MateriasComponent {
   openModal(modal: string) {
     console.log('Modal abierto' + modal);
   }
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: { title: 'nombre_materia' },
+    });
+  }
+
+
 }
